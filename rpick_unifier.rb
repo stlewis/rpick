6e10e450be53c25548671efc3e0364c5ffb5958b
Steve Lewis <steve@yourfuzzy.com>
2021-04-17 10:28:04 -0700

    Fix typos

--- rpick_unifier.rb
+++ rpick_unifier.rb
@@ -1,23 +1,6 @@
 output = ""
-[
-  'rpick_dictionary',
-  'rpick_inventory_management',
-  'rpick_picker',
-  'rpick_ancillary_actions',
-  'rpick_lock_handler',
-  'rpick_trap_handler',
-  'rpick_workflow',
-  'rpick_workflow_customer_boxes',
-  'rpick_workflow_ground_boxes',
-  'rpick_workflow_locksmithpool_boxes',
-  'rpick_workflow_own_boxes',
-  'rpick_setup',
-  'rpick'
-].each do |file|
-  path = "./#{file}.lic"
 
-  contents = File.read(path)
-  header = <<-EOL
+header = <<-EOL
 =begin
     --------------------------------------------------------------------------------
     Title: Rpick
@@ -37,7 +20,26 @@ output = ""
 =end
   EOL
 
-  output << header
+output << header
+
+[
+  'rpick_dictionary',
+  'rpick_inventory_management',
+  'rpick_picker',
+  'rpick_ancillary_actions',
+  'rpick_lock_handler',
+  'rpick_trap_handler',
+  'rpick_workflow',
+  'rpick_workflow_customer_boxes',
+  'rpick_workflow_ground_boxes',
+  'rpick_workflow_locksmithpool_boxes',
+  'rpick_workflow_own_boxes',
+  'rpick_setup',
+  'rpick'
+].each do |file|
+  path = "./#{file}.lic"
+
+  contents = File.read(path)
   output << contents
   output << "\n\n"
 end
@@ -45,4 +47,3 @@ path = File.expand_path(File.dirname(__FILE__) + '/../scripts/rpick.lic')
 ui_path = File.expand_path(File.dirname(__FILE__) + '/../scripts/rpick.ui')
 File.write(path, output)
 File.write(ui_path, File.read('./Rpick.ui'))
-
